4cd1f3d23bc8c4cc0790c7f6f4fa8111274e11ee
PyrotechnicCake <onetruejake@outlook.com>
2020-03-10 18:22:53 +1100

    added xlooping

---
+++ Joust prototype/Assets/Scripts/Xlooping.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xlooping : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.gameObject.transform.position.x <= -10)
+        {
+            transform.Translate(20, 0, 0);
+        }
+        if (this.gameObject.transform.position.x >= 10)
+        {
+            transform.Translate(-20, 0, 0);
+        }
+    }
+}
