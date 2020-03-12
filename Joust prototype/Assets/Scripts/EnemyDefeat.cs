6cb8caf7c6b61fd5409e7de6b291412b4c51e914
PyrotechnicCake <onetruejake@outlook.com>
2020-03-12 16:36:11 +1100

    Added several features
    *made it so the player can defeat enemies
    *made it so enemies emit a particle effect when defeated
    *made a game manager with a score variable
    *made a UI object that will be used to displace changes in score

---
+++ Joust prototype/Assets/Scripts/EnemyDefeat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDefeat : MonoBehaviour
+{
+    public GameObject explodeEffect;
+    List<Collider> colliders = new List<Collider>();
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Player")
+        {
+            if (this.gameObject.transform.position.y + 0.1f < GameObject.FindGameObjectWithTag("Player").transform.position.y)
+            {
+                GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().score += 100;
+                Instantiate(explodeEffect, transform.position, Quaternion.identity);
+                Destroy(gameObject);
+            }
+            print("collision");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
