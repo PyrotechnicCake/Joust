88f98dcca66c2f612cb95086cd1f7dc1afa4c85f
LukeStanley <1015748@student.sae.edu.au>
2020-03-13 10:41:04 +1100

    Player can die

---
+++ Joust prototype/Assets/Scripts/PlayerDefeated.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDefeated : MonoBehaviour
+{
+    public GameObject explodeEffect;
+    List<Collider> colliders = new List<Collider>();
+    public GameManager gameManager;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.tag == "Enemy")
+        {
+            if (this.gameObject.transform.position.y + 0.1f < collision.transform.position.y)
+            {
+                gameManager.score -= 100;
+                Instantiate(explodeEffect, transform.position, Quaternion.identity);
+                transform.position = GameObject.FindGameObjectWithTag("Respawn").transform.position;
+            }
+            print("collision");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+    }// Start is called before the first frame update
+    
+}
