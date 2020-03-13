f5be110cd5802f853ccd9e4adaa3bb12ae90df33
LukeStanley <1015748@student.sae.edu.au>
2020-03-13 17:25:01 +1100

    Movement tweaks

--- Joust prototype/Assets/Scripts/PlayerMovement.cs
+++ Joust prototype/Assets/Scripts/PlayerMovement.cs
@@ -8,61 +8,77 @@ public class PlayerMovement : MonoBehaviour
     public float Speed = 5f;
     public bool onGround = false;
     public GameObject player;
-    private Vector3 touchPosition;
-    private Vector3 direction;
-    private Touch touch;
+    private Vector2 touchPosition;
+    //private Vector3 direction;
+    //private Touch touch;
     public Joystick joystick;
     float horizontalMove = 0f;
 
 
     void FixedUpdate()
     {
-        Jump();
-        //Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        //transform.position += movement * Time.deltaTime * Speed;
-        if (touch.phase == TouchPhase.Moved)
+        if (Input.touchCount > 0)
         {
-            /*touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0;
-            direction = (touchPosition - transform.position);
-            Vector3 touchMovement = new Vector3(direction.x,0) * Speed;*/
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+            {
+                touchPosition = touch.position;
+            }
+            if (touch.phase == TouchPhase.Moved)
+            {
+                /*touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+                touchPosition.z = 0;
+                direction = (touchPosition - transform.position);
+                Vector3 touchMovement = new Vector3(direction.x,0) * Speed;*/
 
-            Vector2 playerScreen = Camera.main.WorldToScreenPoint(transform.position);
-            direction = ((Vector2)touchPosition - playerScreen).normalized;
-            Vector2 touchMovement = new Vector2(direction.x * Time.deltaTime * Speed, transform.position.y);
-            transform.position = touchMovement;
+                //Vector2 playerScreen = Camera.main.WorldToScreenPoint(transform.position);
+                Vector2 direction = ((Vector2)touch.position - touchPosition).normalized;
+                Vector2 touchMovement = new Vector2(direction.x * Time.deltaTime * Speed, transform.position.y);
+                transform.position = touchMovement;
+            }
         }
-        /*if(joystick.Horizontal >= .2f)
+        
+        if (Input.touchCount > 1)
         {
-            horizontalMove = Speed;
+            Jump();
+        }
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+        
+        /*if (joystick.Horizontal >= .2f)
+        {
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * Speed;
         }
         else if (joystick.Horizontal <= -.2f)
         {
-            horizontalMove = -Speed;
+            Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+            transform.position += movement * Time.deltaTime * Speed;
         }
         else
         {
             horizontalMove = 0;
         }*/
+        
     }
     void Jump()
     {
+
+        
+        Vector2 CurrentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+        CurrentVelocity.y = 0f;
+        gameObject.GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
+        gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
+
         if (Input.GetButtonDown("Jump") && onGround == true || Input.GetButtonDown("Jump"))
         {
-            Vector2 CurrentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-            CurrentVelocity.y = 0f;
-            gameObject.GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
+            Vector2 CurVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+            CurVelocity.y = 0f;
+            gameObject.GetComponent<Rigidbody2D>().velocity = CurVelocity;
             // gameObject.GetComponent<Rigidbody2D>().velocity.y = (gameObject.GetComponent<Rigidbody2D>().velocity.x,0)
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
         }
-        if (Input.touchCount > 0)
-        {
-            touch = Input.GetTouch(0);
-            Vector2 CurrentVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
-            CurrentVelocity.y = 0f;
-            gameObject.GetComponent<Rigidbody2D>().velocity = CurrentVelocity;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse);
-        }
+
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
